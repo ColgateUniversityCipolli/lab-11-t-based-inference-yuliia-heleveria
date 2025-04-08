630baacbfa11370dc5bbaddfbc78c17f92d1ca0b
yuliia-heleveria <148816812+yuliia-heleveria@users.noreply.github.com>
2025-04-08 14:41:59 -0400

    Worked on Part 3

--- Lab11.R
+++ Lab11.R
@@ -35,3 +35,75 @@ fig.data <- read_csv("Fig2Dat.csv")
 #mutate data to get the difference
 fig.data <- fig.data|>
   mutate("difference" = closer_vals - further_vals)
+
+################################################################################
+# Task 3: summarize the data
+################################################################################
+#part a - summarize further data
+further.dat <- fig.data$further_vals
+
+#do numerical summary for the data
+furthest.summary <- tibble(
+  mean = mean(further.dat),
+  sd = sd(further.dat),
+  min = min(further.dat),
+  q1 = quantile(further.dat, probs = 0.25),
+  median = quantile(further.dat, probs = 0.50),
+  q3 = quantile(further.dat, probs = 0.75),
+  max = max(further.dat)
+)
+
+#do graphical summary for the data
+furthest.boxplot <- ggplot(data = tibble(further.dat))+
+  geom_boxplot(aes(x = "", y = further.dat*100),
+               fill = "lightblue")+ #make the boxplot for the data
+  theme_bw()+
+  xlab("Further Data")+
+  ylab("Dopanime percentage change")
+
+
+#part b - summarize closer data
+closer.dat <- fig.data$closer_vals
+
+#do numerical summary for the data
+closer.summary <- tibble(
+  mean = mean(closer.dat),
+  sd = sd(closer.dat),
+  min = min(closer.dat),
+  q1 = quantile(closer.dat, probs = 0.25),
+  median = quantile(closer.dat, probs = 0.50),
+  q3 = quantile(closer.dat, probs = 0.75),
+  max = max(closer.dat)
+)
+
+#do graphical summary for the data
+closer.boxplot <- ggplot(data = tibble(closer.dat))+
+  geom_boxplot(aes(x = "", y = closer.dat*100),
+               fill = "lightblue")+ #make the boxplot for the data
+  theme_bw()+
+  xlab("Closer Data")+
+  ylab("Dopanime percentage change")
+
+#part c - summarize paired difference
+diff.dat <- fig.data$difference
+
+#do numerical summary for the data
+diff.summary <- tibble(
+  mean = mean(diff.dat),
+  sd = sd(diff.dat),
+  min = min(diff.dat),
+  q1 = quantile(diff.dat, probs = 0.25),
+  median = quantile(diff.dat, probs = 0.50),
+  q3 = quantile(diff.dat, probs = 0.75),
+  max = max(diff.dat)
+)
+
+#do graphical summary for the data
+diff.boxplot <- ggplot(data = tibble(diff.dat))+
+  geom_boxplot(aes(x = "", y = diff.dat*100),
+               fill = "lightblue")+ #make the boxplot for the data
+  theme_bw()+
+  xlab("Paired difference")+
+  ylab("Dopanime percentage change")
+
+
